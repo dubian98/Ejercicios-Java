5c9692058945f7fdb843cfdef2727c70c84b1c05
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-02-17 14:08:58 -0500

    Correccion en metodo charAt()

--- Ejercicios/src/ejercicios/Ejercicios.java
+++ Ejercicios/src/ejercicios/Ejercicios.java
@@ -8,7 +8,7 @@ public class Ejercicios {
 
     public static void main(String[] args) {
         String nombre;
-        int edad;
+        int edad, tamaño;
         double estatura;
 
         nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
@@ -16,12 +16,16 @@ public class Ejercicios {
         estatura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su estatura: "));
 
         estatura = estatura * 1000;
+        tamaño = nombre.length();
+        
         
         JOptionPane.showMessageDialog(null, " Nombre: " + toUpperCase(nombre) + 
                 "\n Edad: " + Math.pow(edad, 3) + 
                 "\n Estatura: " + estatura + "mm" + 
                 "\n Raiz Cubica edad: " + Math.cbrt(edad)+
-                "\n Letras del nombre: " + length(nombre));
+                "\n Letras del nombre: " + length(nombre)+ 
+                "\n Primera Letra: " + nombre.charAt(0) +
+                "\n Ultima letra: "+ nombre.charAt(tamaño-1));
     }
     
 }
