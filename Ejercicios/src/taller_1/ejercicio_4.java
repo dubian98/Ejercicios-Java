282ec597ebb7750768447946c0c92d5beae1301d
Sebastian H <sebastianhenaoloaiza@gmail.com>
2021-03-16 01:07:09 -0500

    Avance ejercicio #4

--- Ejercicios/src/taller_1/ejercicio_4.java
+++ Ejercicios/src/taller_1/ejercicio_4.java
@@ -5,8 +5,24 @@ muestre:
 
 package taller_1;
 
+import javax.swing.JOptionPane;
+import java.lang.*;
+
 public class ejercicio_4 {
     public static void main(String[] args) {
+        /*Variables*/
+        int cantidad_numeros =  Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de numeros a trabajar: "));
+        int suma, digitos;   
+        String numero;
+        
+        
+        for(int i = 0; i < cantidad_numeros; i++ ){
+            numero = JOptionPane.showInputDialog("Ingrese un numero: ");
+        
+            digitos = numero.length();
+            
+            JOptionPane.showMessageDialog(null,"Resultados: " + "\nNumero: " + numero + "\nCantidad de digitos: " + digitos + "\nSuma");
+        }
         
     }  
 }
