b11e567c187020fc7ce12e7aea1c89563a3bb135
Dubian Gomez <80360981+dubian98@users.noreply.github.com>
2021-03-18 15:13:35 -0500

    Arreglos estéticos

--- Ejercicios/src/taller_1/Menu.java
+++ Ejercicios/src/taller_1/Menu.java
@@ -5,15 +5,13 @@ import javax.swing.*;
 public class Menu  {
 
 
-
-
-    public static void main(String[] args) {
-
-        Ejercicio_5 cuadrado = new Ejercicio_5();
-        Ejercicio_4 digitosNumeros = new Ejercicio_4();
-        Ejercicio_3 notasEstudiantes = new Ejercicio_3();
-        Ejercicio_2 edadPersonas = new Ejercicio_2();
+    public static void main(String[] args) {     
+        
         Ejercicio_1 notasAsignatura = new Ejercicio_1();
+        Ejercicio_2 edadPersonas = new Ejercicio_2();
+        Ejercicio_3 notasEstudiantes = new Ejercicio_3();
+        Ejercicio_4 digitosNumeros = new Ejercicio_4();
+        Ejercicio_5 cuadrado = new Ejercicio_5();
 
         int opcion;
 
@@ -22,9 +20,7 @@ public class Menu  {
         switch (opcion ) {
             case 1:
                 notasAsignatura.notasAsignatura();
-
                 break;
-
             case 2:
                 edadPersonas.edadPersonas();
                 break;
@@ -33,18 +29,13 @@ public class Menu  {
                 break;
             case 4:
                 digitosNumeros.digitosNumeros();
-
                 break;
             case 5:
                 cuadrado.cuadradoNumero();
                 break;
-
             default:
                 System.out.println("Error, opcion digitada incorrecta... ");
         }
-
-
-
 
     }
 
